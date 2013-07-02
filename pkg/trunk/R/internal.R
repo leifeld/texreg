a3ec9d4bdd347da54ce0db3d3ad5f32d0c379904
leifeld <leifeld@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-07-02 20:50:11 +0000

    Fixed bug with coeftostring function for very small numbers (now using the format function instead of regular expressions). Added an extract.texreg function for easy customization and manipulation of texreg objects. Added extract.brglm function. Replaced extract.Relogit method by new extract.zelig function because the former no longer works. Updated the package vignette by using the JSS article instead of the old vignette.

--- pkg/trunk/R/internal.R
+++ pkg/trunk/R/internal.R
@@ -19,29 +19,18 @@ coeftostring <- function(x, lead.zero = FALSE, digits = 2) {
   if (digits < 0) {
     stop("The number of digits must be 0 or higher.")
   }
-  y <- as.character(round(x, digits))
   
-  actual.digits <- attributes(regexpr("\\.[0-9]+$", y))$match.length - 1
-  if (grepl("\\.", y) == FALSE) { #no decimal places present
-    zeros <- paste(rep("0", digits), collapse = "")
-    y <- paste(y, ".", zeros, sep = "")
-  } else if (grepl("\\.[0-9]$", y) == TRUE && digits > 1) { #only one decimal p.
-    zeros <- paste(rep("0", digits - 1), collapse = "")
-    y <- paste(y, zeros, sep = "")
-  } else if (actual.digits < digits) { #more desired digits than present
-    fill <- digits - actual.digits
-    zeros <- paste(rep("0", fill), collapse = "")
-    y <- paste(y, zeros, sep = "")
-  }
-  if (grepl("\\.$", y) == TRUE) {
-    y <- gsub("\\.$", "", y)
-  }
-  if (lead.zero == FALSE && (grepl("^0",y) == TRUE || grepl("^-0",y) == TRUE)) {
+  y <- format(round(x, digits), nsmall = digits, scientific = FALSE)
+  
+  #if (grepl("\\.0+$", y) == TRUE) {  # very small number
+  #  y <- format(x, nsmall = digits, scientific = TRUE)
+  #} else 
+  if (lead.zero == FALSE && (grepl("^0", y) == TRUE ||  # leading zero
+      grepl("^-0", y) == TRUE)) {
     y <- gsub("0\\.", "\\.", y)
   }
   return(y)
 }
-
 
 # function which conflates a matrix with duplicate row names
 rearrangeMatrix <- function(m) {
