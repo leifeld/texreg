9aadf3f1f0207b9ea44fb67ef3eb68ae59486dc2
Claudia Zucca <c.zucca@exeter.ac.uk>
2018-12-03 16:48:53 +0000

    restart Travis CI

--- R/internal.R
+++ R/internal.R
@@ -1002,7 +1002,6 @@ reorder <- function(mat, new.order) {
   return(new.mat)
 }
 
-
 # compute column width left and right of the decimal separator
 compute.width <- function(v, left = TRUE, single.row = FALSE, bracket = ")") {
   if (single.row == FALSE) {
