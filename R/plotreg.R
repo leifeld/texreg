7384811e861cd4fbc97d4f40489dba9acd4ca120
Claudia Zucca <c.zucca@exeter.ac.uk>
2019-02-21 19:15:50 +0000

    ggplot implementation V.2 almost there

--- R/plotreg.R
+++ R/plotreg.R
@@ -1,136 +1,3 @@
-# # function which plots coefficients and confidence intervals ('coefplot')
-# #coefplot <- function(labels, estimates, lower.inner = NULL, 
-#     upper.inner = NULL, lower.outer = NULL, upper.outer = NULL, 
-#     signif.outer = TRUE, xlab = "Coefficients and confidence intervals", 
-#     main = "Coefficient plot", xlim = NULL, cex = 2.5, lwd.zerobar = 4, 
-#     lwd.vbars = 1, lwd.inner = 7, lwd.outer = 5, ylab.cex = 1.0, 
-#     signif.light = "#fbc9b9", signif.medium = "#f7523a", 
-#     signif.dark = "#bd0017", insignif.light = "#c5dbe9", 
-#     insignif.medium = "#5a9ecc", insignif.dark = "#1c5ba6", ...) {
-#   
-#   # check consistency of arguments
-#   if (length(table(c(length(estimates), length(lower.outer), 
-#       length(upper.outer), length(labels)))) > 1) {
-#     stop("Vectors should have the same length.")
-#   }
-#   
-#   # define shortcut variables outer, mn, mx, steps, and num
-#   if (!is.null(lower.outer) && !is.null(upper.outer)) {
-#     outer <- TRUE
-#   } else {
-#     outer <- FALSE
-#   }
-#   if (!is.null(lower.inner) && !is.null(upper.inner)) {
-#     inner <- TRUE
-#   } else {
-#     inner <- FALSE
-#   }
-#   if (outer == TRUE) {
-#     mn <- min(lower.outer)
-#     mx <- max(upper.outer)
-#   } else if (inner == TRUE) {
-#     mn <- min(lower.inner)
-#     mx <- max(upper.inner)
-#   } else {
-#     stop("Either inner or outer bounds must be provided.")
-#   }
-#   if (mn > 0) {
-#     mn <- 0
-#   }
-#   if (mx < 0) {
-#     mx <- 0
-#   }
-#   if (!is.null(xlim) && length(xlim) == 2 && is.numeric(xlim)) {
-#     mn <- xlim[1]
-#     mx <- xlim[2]
-#   }
-#   steps <- floor(mn):ceiling(mx)
-#   num <- length(labels)
-#   
-#   
-#   # which terms are significant?
-#   if (class(signif.outer) == "logical" && 
-#       length(signif.outer) == length(estimates)) {
-#     signif <- signif.outer == FALSE
-#   } else if (outer == TRUE && signif.outer == TRUE) {
-#     signif <- apply(cbind(lower.outer, upper.outer), 1, 
-#         function(x) x[1] <= 0 && x[2] >= 0)
-#   } else if (inner == TRUE && signif.outer == FALSE) {
-#     signif <- apply(cbind(lower.inner, upper.inner), 1, 
-#         function(x) x[1] <= 0 && x[2] >= 0)
-#   } else {
-#     stop("signif.outer does not correspond to the intervals provided.")
-#   }
-#   signif <- signif == FALSE
-#   
-#   # create plot; compute left margin; add labels to left margin
-#   inches.to.lines <- (par("mar") / par("mai"))[1]
-#   lab.width <- max(strwidth(labels, units = "inches") * inches.to.lines * ylab.cex)
-#   
-#   par(mar = c(5, 1 + lab.width, 4, 2) + 0.1)
-#   plot(0, 0, xlim = c(mn, mx), ylim = c(1, length(labels)), xlab = xlab, 
-#       main = main, ylab = "", axes = FALSE, type = "n", ...)
-#   axis(side = 2, at = 1:num, labels = FALSE, las = 2, 
-#       tck = 0, lty = 0, ...)
-#   if (any(signif)) {
-#     mtext(side = 2, text = labels[which(signif)], at = num + 1 - which(signif), 
-#         col = signif.dark, las = 2, cex = ylab.cex, ...)
-#   }
-#   if (any(!signif)) {
-#     mtext(side = 2, text = labels[which(!signif)], 
-#         at = num + 1 - which(!signif), col = insignif.dark, las = 2, 
-#         cex = ylab.cex, ...)
-#   }
-#   axis(side = 1, las = 0, lty = 0, ...)
-#   
-#   # add vertical gray lines
-#   zeros <- rep(0, length(steps))
-#   ends <- rep((num + 1), length(steps))
-#   segments(steps, zeros, steps, ends, col = "gray", lwd = lwd.vbars)
-#   segments(0, 0, 0, num + 1, lwd = lwd.zerobar, col = "grey75")
-#   
-#   # draw outer CIs
-#   if (outer == TRUE) {
-#     if (any(signif)) {
-#       segments(lower.outer[signif], num + 1 - which(signif), 
-#           upper.outer[signif], num + 1 - which(signif), lwd = lwd.outer, 
-#           col = signif.light)
-#     }
-#     if (any(!signif)) {
-#       segments(lower.outer[!signif], num + 1 - which(!signif), 
-#           upper.outer[!signif], num + 1 - which(!signif), lwd = lwd.outer, 
-#           col = insignif.light)
-#     }
-#   }
-#   
-#   # draw inner CIs
-#   if (inner == TRUE) {
-#     if (any(signif)) {
-#       segments(lower.inner[signif], num + 1 - which(signif), 
-#           upper.inner[signif], num + 1 - which(signif), lwd = lwd.inner, 
-#           col = signif.medium)
-#     }
-#     if (any(!signif)) {
-#       segments(lower.inner[!signif], num + 1 - which(!signif), 
-#           upper.inner[!signif], num + 1 - which(!signif), lwd = lwd.inner, 
-#           col = insignif.medium)
-#     }
-#     if (outer == FALSE) {
-# 
-#     }
-#   }
-#   
-#   # draw point estimates
-#   if (any(signif)) {
-#     points(estimates[signif], num + 1 - which(signif), col = signif.dark, 
-#         pch = 20, cex = cex)
-#   }
-#   if (any(!signif)) {
-#     points(estimates[!signif], num + 1 - which(!signif), col = insignif.dark, 
-#         pch = 20, cex = cex)
-#   }
-# }
-# 
 
 # plotreg function
 plotreg <- function(l, file = NULL, custom.model.names = NULL, 
@@ -292,6 +159,7 @@ plotreg <- function(l, file = NULL, custom.model.names = NULL,
     #    dataframe <- dataframe[grep(omit.coef, dataframe$co.names, invert = TRUE), ]
     #}
     #cat("\ndataframe: ", deparse(dataframe))
+    
     dataframe$co <- as.numeric(as.character(dataframe$co))
     
     if (length(dataframe$se) > 0) {
@@ -314,7 +182,7 @@ plotreg <- function(l, file = NULL, custom.model.names = NULL,
     } else {
         #do  nothing
     }
-    cat("\ndataframe: ", deparse(dataframe))
+    #cat("\ndataframe: ", deparse(dataframe))
     
     # aggregate confidence intervals or SEs
     if (use.se[i] == TRUE && length(models[[i]]@se) > 0) {
@@ -390,8 +258,18 @@ plotreg <- function(l, file = NULL, custom.model.names = NULL,
     signif <- signif == FALSE
     
     dataframe <- cbind(dataframe, signif)
+    cat("\ndataframe$co.names1: ", dataframe$signif)
+    rownames(dataframe) <- factor(rownames(dataframe))
+    cat("\ndataframe$co.names2: ", dataframe$signif)
+    #dataframe <- dataframe[order(rownames(dataframe)), , drop = TRUE]
+    if (length(co) == 0) {
+        stop(paste("No coefficients available. Was the 'omit.coef' argument", 
+                   "misspecified? If coefficients were renamed using the",
+                   "'custom.coef.names' argument, 'omit.coef' refers to the renamed",
+                   "coefficients."))
+    }
     
-    #place ggplot function below  
+    # ggplot functions   
     if (type == "facet") {
         p <- ggplot(dataframe, aes(co.names, co)) + 
             geom_hline(yintercept=0, lty=2, lwd=1, colour="grey50") +
@@ -401,9 +279,9 @@ plotreg <- function(l, file = NULL, custom.model.names = NULL,
                           colour = ifelse(signif == TRUE, signif.medium, insignif.medium), width=0) +
             geom_point(size=3, pch = ifelse(signif == TRUE, 21, 22), fill = ifelse(signif == TRUE, signif.dark, insignif.dark)) +
             coord_flip() + 
-            theme_bw()
+            theme_bw() +
+            xlab(" ")
         if (length(models) == 1) {
-            #cat(length(models))
             p <- p +   ggtitle("Model")
         } else if (length(models) > 1) {
             p <- p +  facet_wrap(~lab, strip.position="left", nrow=length(dataframe), scales = "free_y") +
@@ -411,53 +289,52 @@ plotreg <- function(l, file = NULL, custom.model.names = NULL,
         }
         
     } else if (type == "forest") {
-        cat("\ndataframeforest: ", deparse(dataframe))
-        p <- ggplot(dataframe, aes(lab, co)) + 
-            geom_hline(yintercept=0, lty=2, lwd=1, colour="grey50") +
-            geom_errorbar(aes(ymin= lower.inner, ymax= upper.inner), 
-                          lwd=1, colour = ifelse(signif == TRUE , signif.light, insignif.light), width=0) +
-            geom_errorbar(aes(ymin=co - se, ymax=co + se), lwd=2.5, 
-                          colour = ifelse(signif == TRUE, signif.medium, insignif.medium), width=0) +
-            geom_point(size = 3, pch = ifelse(signif == TRUE, 21, 22), fill = ifelse(signif == TRUE, signif.dark, insignif.dark)) +
-            coord_flip() + 
-            theme_bw() 
-        if (length(models) == 1) {
-            #cat(length(models))
-            p <- p +   ggtitle("Model")
-        } else if (length(models) > 1) {
-            p <- p +  facet_wrap(~co.names, strip.position="left", nrow=length(dataframe),scales = "free_y") +
-                ggtitle("Models")
-        }
         
-    }
+        cat("\ndataframeforest: ", deparse(dataframe))
+        #dataframe <- dataframe[order(rownames(dataframe)), ]
+        #dataframe$signif <- as.factor(dataframe$signif)
+            p <- ggplot(dataframe, aes(lab, co)) + 
+                geom_hline(yintercept=0, lty=2, lwd=1, colour="grey50") +
+                geom_errorbar(aes(ymin=lower.outer, ymax=upper.outer), 
+                              lwd=1, colour = ifelse(dataframe[order(rownames(dataframe)) ,]$signif == TRUE , signif.light, insignif.light), width=0) +
+                geom_errorbar(aes(ymin= lower.inner, ymax= upper.inner), lwd=2.5, 
+                              colour = ifelse(dataframe[order(rownames(dataframe)) ,]$signif == TRUE, signif.medium, insignif.medium), width=0) +
+                geom_point(size=3, pch = ifelse(dataframe[order(rownames(dataframe)) ,]$signif == TRUE, 21, 22), fill = ifelse(dataframe[order(rownames(dataframe)) ,]$signif == TRUE, signif.dark, insignif.dark)) +
+                coord_flip() + 
+                theme_bw() +
+                xlab(" ") +
+                facet_wrap(~co.names, strip.position="left", nrow=length(dataframe), scales = "free_y")
+            if (length(models) == 1) {
+                p <- p +   ggtitle("Model")
+            } else if (length(models) > 1) {
+                p <- p +  
+                    ggtitle("Models")
+            }
+            
+    cat("\ndataframe$co.names2: ", dataframe$signif)   
+    } 
+    # else if (type == "forest" && length(models == 1)) {
+    # stop(message("Forest plot can be specified for two or more models only."))
+    # }
     
-    print(p)
-    #meassages to paste
-    # add messages to p and print at the end
+    # adds message to p as ylab; adds output meassages to paste that comes as R output (not in plot)
     if (use.se[i] == TRUE && length(models[[i]]@se) > 0) {
-        note <- "Bars denote SEs."
+        p <- p + ylab("Bars denote SEs.")
         message(paste0("Model ", i, 
                        ": bars denote one (inner) resp. two (outer) standard errors."))
     } else if (length(models[[i]]@ci.low) == 0 && length(models[[i]]@se) > 0) {
-        note <- "Bars denote CIs."
-        message(paste0("Model ", i, ": bars denote 0.5 (inner) resp. ", ci.level, 
-                       " (outer) confidence intervals (computed from standard errors)."))
+        p <- p + ylab("Bars denote CIs.")
+         message(paste0("Model ", i, ": bars denote 0.5 (inner) resp. ", ci.level, 
+                        " (outer) confidence intervals (computed from standard errors)."))
     } else {
-        note <- "Bars denote CIs."
+        p <- p + ylab("Bars denote CIs.")
         message(paste0("Model ", i, ": bars denote  ", ci.level, 
                        " confidence intervals."))
     }
     
-    if (!is.null(custom.note)) {
-        note <- custom.note
-    }
+    print(p)
     
-    if (length(co) == 0) {
-        stop(paste("No coefficients available. Was the 'omit.coef' argument", 
-                   "misspecified? If coefficients were renamed using the",
-                   "'custom.coef.names' argument, 'omit.coef' refers to the renamed",
-                   "coefficients."))
-    }
+    
     
     if (!is.null(file) && !is.na(file)) {
         dev.off()
