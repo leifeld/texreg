bfa13b0de2dcd676175869dd2c1a4ab46ef019e2
David Hugh-Jones <davidhughjones@gmail.com>
2022-07-03 14:03:45 +0100

    Pass dots to matrixreg in huxtablereg. Fix #192.

--- tests/testthat/test-huxtablereg.R
+++ tests/testthat/test-huxtablereg.R
@@ -19,3 +19,17 @@ test_that("huxtablereg works", {
   skip_if_not_installed("huxtable", minimum_version = "5.0.0")
   expect_equivalent(dim(as.data.frame(huxtablereg(list(m1, m2)))), c(10, 3))
 })
+
+test_that("huxtablereg works with beside = TRUE", {
+  skip_if_not_installed("systemfit")
+
+  library(systemfit)
+  data("Kmenta")
+  eqDemand <- consump ~ price + income
+  eqSupply <- consump ~ price + farmPrice + trend
+  system <- list(demand = eqDemand, supply = eqSupply)
+  fitols <- systemfit(system, data = Kmenta)
+
+  h <- huxtablereg(fitols, beside = TRUE)
+  expect_equal(ncol(h), 3)
+})
